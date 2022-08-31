88399064a2836e9a0f51f4edd9dd950970185eba
Darllinson Azevedo <azevedo.darllinson@gmail.com>
2022-08-31 15:58:58 -0300

    Inital commit

---
+++ src/app/core/components/header/header.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+import { ETheme } from '../../enums/ETheme.enum';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent implements OnInit {
+  public icon: string = ETheme.ICON_MOON;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  public switchTheme() {
+    const theme = document.body.classList.toggle('dark-theme');
+
+    if (theme) {
+      return (this.icon = ETheme.ICON_SUN);
+    }
+
+    return (this.icon = ETheme.ICON_MOON);
+  }
+}
